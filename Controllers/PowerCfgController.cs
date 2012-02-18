f2401099b73174831467a79333c3fb8cf7a0ca2d
Alex Lee <stratrix@gmail.com>
2012-02-18 13:13:13 -0800

    initial import
    Signed-off-by: Alex Lee <stratrix@gmail.com>

---
+++ Controllers/PowerCfgController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace com.alexleekt.aideNotebook
+{
+    class PowerCfgController
+    {
+        public enum PowerSource {
+          AC, DC
+        };
+
+        public static Int32[] PERCENTAGES = { 60, 80, 99, 100 };
+
+        public static String APP_POWERCFG = "powercfg";
+        public static String SUBGROUP_GUID_PROCESSOR_POWER_MANAGEMENT = "54533251-82be-4824-96c1-47b60b740d00";
+        public static String SETTING_GUID_MAXIMUM_PROCESSOR_STATE = "bc5038f7-23e0-4960-96da-33abaf5935ec";
+
+        private static PowerProfile _ActivePowerProfile;
+        public static PowerProfile ActivePowerProfile
+        {
+            get
+            {
+                if (_ActivePowerProfile == null)
+                {
+                    _ActivePowerProfile = GetActiveScheme();
+                }
+                return _ActivePowerProfile;
+            }
+        }
+
+        private static String ExecutePowerCfg(String args)
+        {
+            // http://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results
+            Process p = new Process();
+            p.StartInfo.FileName = APP_POWERCFG;
+            p.StartInfo.Arguments = args;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+            String output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return output;
+        }
+
+        public static PowerProfile GetActiveScheme()
+        {
+            // txt2re.com
+            string txt = ExecutePowerCfg("-l");
+
+            string re1 = ".*?";	// Non-greedy match on filler
+            string re2 = "([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12})";	// SQL GUID 1
+            string re3 = ".*?";	// Non-greedy match on filler
+            string re4 = "((?:[a-z][a-z]+))";	// Word 1
+
+            Regex r = new Regex(re1 + re2 + re3 + re4, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            Match m = r.Match(txt);
+            if (m.Success)
+            {
+                String guid = m.Groups[1].ToString();
+                String name = m.Groups[2].ToString();
+                return new PowerProfile(guid, name);
+            }
+            else
+            {
+                throw new Exception("GetActiveScheme(): Unable to extract Profile GUID and Name");
+            }
+            
+        }
+
+        public static void SetMaximumPowerState(PowerProfile pwrprf, PowerSource pwsrc, int percentage) 
+        {
+            if (pwrprf == null)
+            {
+                pwrprf = ActivePowerProfile;
+            }
+
+            //powercfg -setdcvalueindex [schemeguid] [subgroupguid] [settingguid] [setting]
+            String args = String.Format("{0} {1} {2} {3} {4}", new String[] { 
+                GetSetPowerSourceValueIndexParam(pwsrc), pwrprf.Guid, SUBGROUP_GUID_PROCESSOR_POWER_MANAGEMENT, SETTING_GUID_MAXIMUM_PROCESSOR_STATE, Convert.ToString(percentage) });
+            ExecutePowerCfg(args);
+            SetActiveScheme(pwrprf);
+        }
+
+        public static void SetActiveScheme(PowerProfile pwrprf)
+        {
+            String args =  String.Format("-setactive {0}", pwrprf.Guid);
+            ExecutePowerCfg(args);
+        }
+
+        public static String GetSetPowerSourceValueIndexParam(PowerSource pwsrc)
+        {
+            switch (pwsrc)
+            {
+                case PowerSource.AC:
+                    return "-SETACVALUEINDEX";
+                case PowerSource.DC:
+                    return "-SETDCVALUEINDEX";
+            }
+            return "";
+        }
+
+    }
+}
---
+++ Views/ViewMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace com.alexleekt.aideNotebook
+{
+    public partial class ViewMain : Form
+    {
+        #region Moveable Borderless Window
+        //ref: http://www.codeproject.com/KB/cs/csharpmovewindow.aspx
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+       
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        
+        public ViewMain()
+        {
+            InitializeComponent();
+            PowerCfgController.SetMaximumPowerState(null, PowerCfgController.PowerSource.AC, 100);
+            PowerCfgController.SetMaximumPowerState(null, PowerCfgController.PowerSource.DC, 60);
+            hkm = new HotkeyManager();
+        }
+
+        private void redrawCpuSpeed()
+        {
+            ManagementObject Mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'");
+            uint sp = (uint)(Mo["CurrentClockSpeed"]);
+            Mo.Dispose();
+            this.label2.Text = sp + "MHz";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            redrawCpuSpeed();
+        }
+
+        HotkeyManager hkm;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hkm.RegisterGlobalHotKey(this.Name, this.Handle, Keys.Space, HotkeyManager.MOD_SHIFT);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            hkm.UnregisterGlobalHotKey(this.Handle);
+        }
+
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            // let the base class process the message
+            base.WndProc(ref m);
+
+            // if this is a WM_HOTKEY message, notify the parent object
+            const int WM_HOTKEY = 0x312;
+            if (m.Msg == WM_HOTKEY)
+            {
+                this.label1.Text = "detected";
+            }
+        }
+
+    }
+}
